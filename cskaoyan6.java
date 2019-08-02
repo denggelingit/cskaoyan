7f43078995140ec3e1ac075bf6121290fef7fac3
denggelingit <42577578+denggelingit@users.noreply.github.com>
2019-08-03 03:19:09 +0800

    6 【程序 6 求最大公约数及最小公倍数】

---
+++ cskaoyan6.java
@@ -0,0 +1,41 @@
+package cskaoyan;
+
+public class cskaoyan6 {
+	@org.junit.Test
+	public void gcd_lcm() {
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		int m = in.nextInt();
+		int n = in.nextInt();
+
+		System.out.println(gcd(m, n));
+		System.out.println(lcm(m, n));
+
+		in.close();
+	}
+
+	public int gcd(int m, int n) {// 最大公约数
+		int temp = 0;
+
+		if (m < n) {
+			temp = m;
+			m = n;
+			n = temp;
+		}
+
+		while (n != 0) {
+			temp = m % n;
+			m = n;
+			n = temp;
+		}
+
+		return m;
+	}
+
+	public int lcm(int m, int n) {// 最小公倍数
+		int temp;
+
+		temp = m * n / gcd(m, n);
+
+		return temp;
+	}
+}
