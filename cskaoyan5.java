e3468cce382cecec528b148330a9130b0961af4d
denggelingit <42577578+denggelingit@users.noreply.github.com>
2019-08-03 03:18:27 +0800

    5 【程序 5 判断分数等级】

---
+++ cskaoyan5.java
@@ -0,0 +1,15 @@
+package cskaoyan;
+
+public class cskaoyan5 {
+	@org.junit.Test
+	public void judge() {
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		int score = in.nextInt();
+		System.out.println(grade(score));
+		in.close();
+	}
+
+	public char grade(int score) {
+		return (score >= 90) ? 'A' : ((score >= 60) ? 'B' : 'C');
+	}
+}
